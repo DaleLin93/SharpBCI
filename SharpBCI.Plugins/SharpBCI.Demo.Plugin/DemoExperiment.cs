0bdeb7cf5d6a8aeca1f39a8c46d32aa13f7b2e9a
Maruko <100017417@qq.com>
2019-09-10 18:54:31 +0800

    Add demo experiment.

---
+++ SharpBCI.Plugins/SharpBCI.Demo.Plugin/DemoExperiment.cs
@@ -0,0 +1,72 @@
+﻿using MarukoLib.Lang;
+using MarukoLib.UI;
+using SharpBCI.Core.Experiment;
+using SharpBCI.Extensions;
+using SharpBCI.Extensions.Experiments;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SharpBCI.Experiments.Demo
+{
+
+    [Experiment(ExperimentName, "1.0", Description = "This is a simple demo.")]
+    public class DemoExperiment : Experiment
+    {
+
+        public const string ExperimentName = "Demo";
+
+        /// <summary>
+        /// The factory class of demo experiment.
+        /// </summary>
+        public class Factory : ExperimentFactory<DemoExperiment>
+        {
+
+            /// <summary>
+            /// Text parameter 
+            /// </summary>
+            private static readonly Parameter<string> Text = new Parameter<string>("Text", null, description: "The text to display", defaultValue: "Demo");
+
+            private static readonly Parameter<Color> ForegroundColor = new Parameter<Color>("Foreground Color", null, "The color of text", Color.Red);
+
+            private static readonly Parameter<Color> BackgroundColor = new Parameter<Color>("Background Color", null, "The background color of window", Color.Black);
+
+            public override IReadOnlyCollection<ParameterGroup> ParameterGroups => new[] { new ParameterGroup(Text, ForegroundColor, BackgroundColor), };
+
+            /// <summary>
+            /// Create the demo experiment with given context.
+            /// </summary>
+            /// <param name="context">The context that containing required parameters.</param>
+            /// <returns>The created experiment instance.</returns>
+            public override DemoExperiment Create(IReadonlyContext context)
+            {
+                var experiment = new DemoExperiment();
+                experiment.Text = Text.Get(context);
+                experiment.ForegroundColor = ForegroundColor.Get(context, ColorUtils.ToUIntArgb);
+                experiment.BackgroundColor = BackgroundColor.Get(context, ColorUtils.ToUIntArgb);
+                return experiment;
+            }
+
+        }
+
+        /// <summary>
+        /// Text to display in the window.
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// The color of text.
+        /// </summary>
+        public uint ForegroundColor;
+
+        /// <summary>
+        /// The background color of window.
+        /// </summary>
+        public uint BackgroundColor;
+
+        internal DemoExperiment() : base(ExperimentName) { }
+
+        public override void Run(Session session) => new TestWindow(session, this).ShowDialog();
+
+    }
+
+}
---
+++ SharpBCI.Plugins/SharpBCI.Demo.Plugin/TestWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using SharpBCI.Core.Experiment;
+using SharpBCI.Extensions;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using MarukoLib.UI;
+using SharpBCI.Core.IO;
+
+namespace SharpBCI.Experiments.Demo
+{
+    /// <inheritdoc cref="Window" />
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    internal partial class TestWindow
+    {
+
+        /// <summary>
+        /// Current running session.
+        /// </summary>
+        private readonly Session _session;
+
+        /// <summary>
+        /// Markable interface to record markers during the experiment.
+        /// </summary>
+        private readonly IMarkable _markable;
+
+        public TestWindow(Session session, DemoExperiment experiment)
+        {
+            InitializeComponent();
+
+            _session = session;
+            _markable = session.StreamerCollection.FindFirstOrDefault<IMarkable>();
+
+            /* Set experiment parameters to this window. */
+            CueTextBlock.Text = experiment.Text;
+            CueTextBlock.Foreground = new SolidColorBrush(experiment.BackgroundColor.ToSwmColor());
+            Background = new SolidColorBrush(experiment.BackgroundColor.ToSwmColor());
+        }
+
+        /// <summary>
+        /// Handling Loaded event 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_OnLoaded(object sender, RoutedEventArgs e) => _session.Start();
+
+        private void Window_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape: /* Quit this experiment */
+                    _markable?.Mark(MarkerDefinitions.UserExitMarker);
+                    Stop(true);
+                    break;
+            }
+        }
+
+        private void Stop(bool userInterrupted = false)
+        {
+            Close();
+            _session.Finish(userInterrupted);
+        }
+
+    }
+}
