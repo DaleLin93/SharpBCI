bd3ea1dcc23bf6e104564e91bbb4f654d64a3834
Lin Xinyuan <100017417@qq.com>
2019-11-09 00:03:28 +0800

    Fix marker parallel port writer to ensure usability after parallel port write test.

--- SharpBCI.Extensions/IO/Consumers/MarkerParallelPortWriter.cs
+++ SharpBCI.Extensions/IO/Consumers/MarkerParallelPortWriter.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices;
 using MarukoLib.Interop;
 using MarukoLib.Lang;
+using MarukoLib.Lang.Concurrent;
 using SharpBCI.Core.Experiment;
 using SharpBCI.Core.IO;
 using SharpBCI.Extensions.IO.Devices.MarkerSources;
@@ -8,8 +12,17 @@ using SharpBCI.Extensions.IO.Devices.MarkerSources;
 namespace SharpBCI.Extensions.IO.Consumers
 {
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public enum LogicalParallelPort : byte
+    {
+        LPT1 = 1,
+        LPT2 = 2,
+        LPT3 = 3,
+        LPT4 = 4
+    }
+
     [StreamConsumer(ConsumerName, typeof(Factory), "1.0")]
-    public class MarkerParallelPortWriter : StreamConsumer<Timestamped<IMarker>>
+    public class MarkerParallelPortWriter : StreamConsumer<Timestamped<IMarker>>, IDisposable
     {
 
         public const string ConsumerName = "Marker Parallel Port Writer";
@@ -17,14 +30,7 @@ namespace SharpBCI.Extensions.IO.Consumers
         public class Factory : StreamConsumerFactory<Timestamped<IMarker>>
         {
 
-            public static readonly ITypeConverter UShort2HexConverter =
-                TypeConverter<ushort, string>.Of(num => $"0x{num:X}", hex => Convert.ToUInt16(hex, 16));
-
-            public static readonly Parameter<ushort> ParallelPortAddressParam = Parameter<ushort>
-                .CreateBuilder("Parallel Port Address")
-                .SetSelectableValues(new ushort[] { 0x278, 0x378, 0x3BC })
-                .SetMetadata(Presenters.Presenters.PresentTypeConverterProperty, UShort2HexConverter)
-                .Build();
+            public static readonly Parameter<LogicalParallelPort> ParallelPortAddressParam = Parameter<LogicalParallelPort>.OfEnum("LPT", LogicalParallelPort.LPT1);
 
             public Factory() : base(ParallelPortAddressParam) { }
 
@@ -33,18 +39,36 @@ namespace SharpBCI.Extensions.IO.Consumers
 
         }
 
-        private readonly IntPtr _portAddress;
+        private readonly AtomicPtr _fileHandle;
 
-        public MarkerParallelPortWriter(uint portAddress) => _portAddress = (IntPtr)portAddress;
+        private IntPtr _oneByteBuffer;
+
+        public MarkerParallelPortWriter(LogicalParallelPort lpt)
+        {
+            Port = lpt;
+            _fileHandle = new AtomicPtr(Kernel32.CreateFile($"lpt{(byte)lpt}", FileAccess.ReadWrite, FileShare.None, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero));
+            _oneByteBuffer = Marshal.AllocHGlobal(1);
+        }
+
+        public LogicalParallelPort Port { get; }
 
         public bool SendEvent(byte b)
         {
-            var written = 0;
-            Kernel32.OVERLAPPED overlapped = null;
-            return Kernel32.WriteFile(_portAddress, new[] { b }, 1, ref written, ref overlapped) && written > 0;
+            var fd = _fileHandle.Value;
+            if (fd == IntPtr.Zero) return false;
+            return Kernel32.WriteFile(fd, _oneByteBuffer, 1, out var written, IntPtr.Zero) && written > 0;
         }
 
         public override void Accept(Timestamped<IMarker> value) => SendEvent((byte)(value.Value.Code & 0xFF));
+
+        public void Dispose()
+        {
+            var fd = _fileHandle.Value;
+            if (fd == IntPtr.Zero || !_fileHandle.CompareAndSet(fd, IntPtr.Zero)) return;
+            Kernel32.CloseHandle(fd);
+            Marshal.FreeHGlobal(_oneByteBuffer);
+            _oneByteBuffer = IntPtr.Zero;
+        }
 
     }
 }
