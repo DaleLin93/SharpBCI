aa01da22be237becc67cb1c1d3fb4ed494c5f72c
Lin Xinyuan <100017417@qq.com>
2019-11-08 23:09:19 +0800

    Update build-tool.bat

--- Build/build-tool.bat
+++ Build/build-tool.bat
@@ -1,4 +1,11 @@
 @echo off
+setlocal enableextensions enabledelayedexpansion
+call :getargc argc %*
+if %argc% equ 0 (
+  echo no args
+  goto :eof
+)
+endlocal
 set projectDir=%1
 set projectDir=%projectDir:~1,-1%
 set outDir=%2
@@ -14,3 +21,16 @@ xcopy "%sourceDir%*" "%targetDir%" /C /Y
 if %configurationName%==Release (
 	del "%targetDir%*.pdb" /Q
 )
+goto :eof
+
+:getargc
+    set getargc_v0=%1
+    set /a "%getargc_v0% = 0"
+:getargc_l0
+    if not x%2x==xx (
+        shift
+        set /a "%getargc_v0% = %getargc_v0% + 1"
+        goto :getargc_l0
+    )
+    set getargc_v0=
+    goto :eof
