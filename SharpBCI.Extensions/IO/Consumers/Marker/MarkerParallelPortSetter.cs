68a508f044e9d6b5acb530563e776b07c21582e4
Lin Xinyuan <100017417@qq.com>
2019-11-13 00:05:39 +0800

    Make ParallelPortAddress selectable and further customizable.

--- SharpBCI.Extensions/IO/Consumers/Marker/MarkerParallelPortSetter.cs
+++ SharpBCI.Extensions/IO/Consumers/Marker/MarkerParallelPortSetter.cs
@@ -5,6 +5,7 @@ using MarukoLib.Lang;
 using SharpBCI.Core.Experiment;
 using SharpBCI.Core.IO;
 using SharpBCI.Extensions.IO.Devices.MarkerSources;
+using SharpBCI.Extensions.Presenters;
 
 namespace SharpBCI.Extensions.IO.Consumers.Marker
 {
@@ -22,14 +23,19 @@ namespace SharpBCI.Extensions.IO.Consumers.Marker
                 num => $"0x{num:X}", str => Convert.ToInt16(str, str.StartsWith("0x") ? 16 : 10));
 
             public static readonly Parameter<short> ParallelPortAddressParam = Parameter<short>.CreateBuilder("Port Address", 0x378)
+                .SetMetadata(Presenters.Presenters.PresenterProperty, TypeConvertedPresenter.Instance)
                 .SetMetadata(Presenters.Presenters.PresentTypeConverterProperty, Short2HexConverter)
+                .SetMetadata(TypeConvertedPresenter.ConvertedContextProperty, new ContextBuilder()
+                    .SetProperty(SelectablePresenter.CustomizableProperty, true)
+                    .BuildReadonly())
+                .SetSelectableValues(new short[] {0x378, 0x278, 0x3BC})
                 .Build();
 
             public static readonly Parameter<bool> InvertBitsParam = MarkerParallelPortWriter.Factory.InvertBitsParam;
 
             public static readonly Parameter<bool> ReverseBitsParam = MarkerParallelPortWriter.Factory.ReverseBitsParam;
 
-            public Factory() : base(ParallelPortAddressParam, ReverseBitsParam) { }
+            public Factory() : base(ParallelPortAddressParam, InvertBitsParam, ReverseBitsParam) { }
 
             public override IStreamConsumer<Timestamped<IMarker>> Create(Session session, IReadonlyContext context, byte? num)
                 => new MarkerParallelPortSetter(ParallelPortAddressParam.Get(context), InvertBitsParam.Get(context), ReverseBitsParam.Get(context));
