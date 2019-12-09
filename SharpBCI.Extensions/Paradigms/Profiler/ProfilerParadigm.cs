e81d961fa4e7921b9081a7ad5248bbbfc7a3fdc6
Lin Xinyuan <100017417@qq.com>
2019-12-10 01:03:49 +0800

    Add ProfilerParadigm.

---
+++ SharpBCI.Extensions/Paradigms/Profiler/ProfilerParadigm.cs
@@ -0,0 +1,28 @@
+﻿using MarukoLib.Lang;
+using SharpBCI.Core.Experiment;
+
+namespace SharpBCI.Extensions.Paradigms.Profiler
+{
+
+    [Paradigm(ParadigmName, typeof(Factory), "Diagnostics", "0.0.1", Description = "A I/O profiler tool for system diagnostics.")]
+    public class ProfilerParadigm : Paradigm
+    {
+        
+        public const string ParadigmName = "Profiler";
+
+        public static readonly ProfilerParadigm Instance = new ProfilerParadigm();
+
+        public class Factory : ParadigmFactory<ProfilerParadigm>
+        {
+
+            public override ProfilerParadigm Create(IReadonlyContext context) => Instance;
+
+        }
+
+        private ProfilerParadigm() : base(ParadigmName) { }
+
+        public override void Run(Session session) => new ProfilerWindow(session).ShowDialog();
+
+    }
+
+}
